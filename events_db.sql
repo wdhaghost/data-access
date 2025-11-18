36f11ffbbeedac787783b92946fe783c92b6d7e0
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2025-11-18 16:28:02 +0100

    db events

---
+++ events_db.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE IF NOT EXISTS events_db;
+
+USE events_db;
+
+CREATE TABLE event (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    max_attendees INT NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE attendee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    event_id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE
+);
+
+--PROCEDURES
+
+--1 Créer un évènement
+
+--2 Inscription à un évènement (échec si max pers)
+
+--3 Désinscription avec nom prénom
+
+--4 Suppr un évènement avec toutes les inscriptions
+
+--5 changer la date de début et de fin
