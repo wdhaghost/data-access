b2a89e0eb35845f13a97d28713d5d39f0046313f
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2025-11-18 17:02:10 +0100

    feat(procedure): procedure création event et attendee

--- sql/events_db.sql
+++ sql/events_db.sql
@@ -10,6 +10,7 @@ CREATE TABLE event (
     max_attendees INT NOT NULL,
     location VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP NULL
 )
 
 CREATE TABLE attendee (
@@ -22,13 +23,24 @@ CREATE TABLE attendee (
 );
 
 --PROCEDURES
-
 --1 Créer un évènement
-
+DELIMITER //;
+CREATE PROCEDURE create_events(name CHAR(50),start_date DATE,end_date DATE,location VARCHAR(100),max_attendees INT)
+    BEGIN
+        INSERT into event VALUES (name,start_date,end_date,max_attendees,location);
+    END //
+DELIMITER ;
 --2 Inscription à un évènement (échec si max pers)
-
+DELIMITER //;
+CREATE PROCEDURE add_attendee(event_id INT, fn VARCHAR(30),ln VARCHAR(30))
+    BEGIN
+        INSERT into attendee VALUES (event_id,)
+    END
+DELIMITER ;
 --3 Désinscription avec nom prénom
 
 --4 Suppr un évènement avec toutes les inscriptions
 
 --5 changer la date de début et de fin
+
+DELIMITER ;
