d2c29b81cc4d361f0476a3f7b690bb2acdc094d4
Mahery <mahery.gonin@gmail.com>
2025-11-19 15:19:33 +0100

    feat: add insert to mongodb

--- backend/read-json.js
+++ backend/read-json.js
@@ -1,4 +1,3 @@
-
 const readJsonFile = () => {
 
     const filePath = process.argv[2];
@@ -15,5 +14,23 @@ const readJsonFile = () => {
     }
 }
 
-console.log(readJsonFile());
+const insertInMongoDB = (json_data) => {
+    let Mongodb = require('mongodb').MongoClient;
 
+    Mongodb.connect("mongodb://localhost:27017/", function (err, db) {
+        if (err) throw err;
+        let dbo = db.db("mydb");
+
+        json_data.forEach(item => {
+            dbo.collection("events").insertOne(item, function (err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+            });
+        });
+
+        db.close();
+    });
+}
+
+
+insertInMongoDB(readJsonFile())
