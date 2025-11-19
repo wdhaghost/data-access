2e6d1dcc518e4225bc30ab0107276a9f883c49b4
gauthierauge <gauthier.auge@2dcom.fr>
2025-11-19 12:24:37 +0100

    fix: correction path

--- sql/config/update_procedures.sh
+++ sql/config/update_procedures.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "=== Suppression des anciennes procédures ==="
-mysql -u root -p < 03_drop.sql
+mysql -u root -p < ../03_drop.sql
 
 echo "=== Installation des nouvelles procédures ==="
-mysql -u root -p < 04_procedures.sql
+mysql -u root -p < ../04_procedures.sql
 
 echo "✅ Mise à jour terminée!"
