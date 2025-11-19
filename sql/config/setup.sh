73fe004633e9ac9525924eb5e1a7bf1d7ad1d545
gauthierauge <gauthier.auge@2dcom.fr>
2025-11-19 12:35:53 +0100

    feat: add mysql config

--- sql/config/setup.sh
+++ sql/config/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "=== Création de la base de données et des tables ==="
-mysql -u root -p < ../01_schema.sql
+mysql --defaults-extra-file=mysql.cnf < ../01_schema.sql
 
 echo "=== Création des utilisateurs ==="
-mysql -u root -p < ../02_users.sql
+mysql --defaults-extra-file=mysql.cnf < ../02_users.sql
 
 echo "=== Installation des procédures et fonctions ==="
-mysql -u root -p < ../04_procedures.sql
+mysql --defaults-extra-file=mysql.cnf < ../04_procedures.sql
 
 echo "✅ Installation complète terminée!"
--- sql/config/update_procedures.sh
+++ sql/config/update_procedures.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "=== Suppression des anciennes procédures ==="
-mysql -u root -p < ../03_drop.sql
+mysql --defaults-extra-file=mysql.cnf < ../03_drop.sql
 
 echo "=== Installation des nouvelles procédures ==="
-mysql -u root -p < ../04_procedures.sql
+mysql --defaults-extra-file=mysql.cnf < ../04_procedures.sql
 
 echo "✅ Mise à jour terminée!"
