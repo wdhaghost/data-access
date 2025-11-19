f7147f175a032cb3202ac382a63339295393e9f0
hich-ham-dev <hicham.hamza.dev@gmail.com>
2025-11-19 11:47:34 +0100

    fix: syntax

--- sql/events_db.sql
+++ sql/events_db.sql
@@ -19,18 +19,18 @@ CREATE TABLE IF NOT EXISTS attendee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE
+    FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE
 );
 
---Utilisateurs
-CREATE USER 'johndoe' @ 'localhost' IDENTIFIED BY 'password';
+-- Utilisateurs
+CREATE USER 'johndoe'@'localhost' IDENTIFIED BY 'password';
 
-GRANT SELECT, CALL ON events_db.* TO 'johndoe' @ 'localhost';
+GRANT SELECT, EXECUTE ON events_db.* TO 'johndoe'@'localhost';
 
 FLUSH PRIVILEGES;
 
---PROCEDURES
---1 Créer un évènement
+-- PROCEDURES
+-- 1 Créer un évènement
 DELIMITER //
 
 CREATE PROCEDURE create_events(new_name CHAR(50), new_start_date DATE, new_end_date DATE, new_location VARCHAR(100), new_max_attendees INT)
@@ -39,58 +39,44 @@ CREATE PROCEDURE create_events(new_name CHAR(50), new_start_date DATE, new_end_d
         VALUES (new_name, new_start_date, new_end_date, new_max_attendees, new_location);
     END//
 
-DELIMITER ;
-
---2 Inscription à un évènement (échec si max pers)
-DELIMITER //
-
+-- 2 Inscription à un évènement (échec si max pers)
 CREATE FUNCTION is_full(actual_attendees INT, limit_attendees INT)
-    RETURNS VARCHAR(50)
+    RETURNS BOOLEAN
     DETERMINISTIC
     BEGIN
-        RETURN actual_attendees < limit_attendees
+        RETURN actual_attendees >= limit_attendees;
     END//
 
-DELIMITER ;
-
-DELIMITER //
-
-CREATE PROCEDURE add_attendee(event_id INT, fn VARCHAR(30),ln VARCHAR(30))
+CREATE PROCEDURE add_attendee(p_event_id INT, fn VARCHAR(30),ln VARCHAR(30))
     BEGIN
 
     DECLARE limit_attendees INT;
     DECLARE actual_attendees INT;
 
-    SELECT max_attendees INTO limit_attendees FROM event WHERE id = event_id;
-    SELECT COUNT(*) INTO actual_attendees FROM attendee WHERE event_id = event_id;
+    SELECT max_attendees INTO limit_attendees FROM event WHERE id = p_event_id;
+    SELECT COUNT(*) INTO actual_attendees FROM attendee WHERE attendee.event_id = p_event_id;
 
     IF NOT is_full(actual_attendees, limit_attendees) THEN
-        INSERT into attendee VALUES (event_id,fn,ln);
+        INSERT into attendee(event_id, first_name, last_name) VALUES (event_id,fn,ln);
+        SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = "Il y a encore des places";
     ELSE
+        SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = "Evènement plein";
-    END IF
+    END IF;
     END//
 
-DELIMITER ;
-
---3 Désinscription avec nom prénom
-DELIMITER //
-
-CREATE PROCEDURE delete_attendee(event_id INT, fn VARCHAR(30),ln VARCHAR(30))
+-- 3 Désinscription avec nom prénom
+CREATE PROCEDURE delete_attendee(p_event_id INT, fn VARCHAR(30),ln VARCHAR(30))
     BEGIN
         DELETE FROM attendee 
         WHERE first_name = fn
             AND last_name = ln 
-            AND event_id = event_id 
+            AND event_id = p_event_id
         LIMIT 1 ; 
     END//
 
-DELIMITER ;
-
---4 Suppr un évènement avec toutes les inscriptions
-DELIMITER //
-
+-- 4 Suppr un évènement avec toutes les inscriptions
 CREATE PROCEDURE delete_event(event_id INT)
     BEGIN
         DELETE FROM event 
@@ -98,21 +84,15 @@ CREATE PROCEDURE delete_event(event_id INT)
         LIMIT 1 ; 
     END//
 
-DELIMITER ;
-
-DELIMITER //
-
-CREATE TRIGGER 'delete_attendees' AFTER DELETE ON 'event'
+CREATE TRIGGER 'delete_attendees'
+    AFTER DELETE ON 'event'
     FOR EACH ROW
     BEGIN
         DELETE FROM attendee 
         WHERE event_id = OLD.id;
     END//
-DELIMITER ;
 
---5 changer la date de début et de fin
-DELIMITER //
-
+-- 5 changer la date de début et de fin
 CREATE PROCEDURE update_date(event_id INT,new_start_date DATE ,new_end_date DATE)
     BEGIN
         UPDATE event 
